7c34a0f233e511541b3a336efdc18d54482e70b4
candybox0510@naver.com <candybox0510@naver.com>
2018-11-09 17:11:11 +0900

    first commit

---
+++ src/com/amc/service/alarm/AlarmDAO.java
@@ -0,0 +1,22 @@
+package com.amc.service.alarm;
+
+import java.util.List;
+
+import com.amc.common.Search;
+import com.amc.service.domain.Alarm;
+
+public interface AlarmDAO {
+
+public void addCancelAlarm(Alarm cancelAlarm);
+	
+	public void addOpenAlarm(Alarm openAlarm);
+	
+	public List<Alarm> getCancelAlarmList(Search search);
+	
+	public List<Alarm> getOpenAlarmList(Search search);
+	
+	public void deleteCancelAlarm(int cancelAlarmNo);
+	
+	public void deleteOpenAlarm(int openAlarmNo); 
+	
+}
---
+++ src/com/amc/service/alarm/impl/AlarmDAOImpl.java
@@ -0,0 +1,49 @@
+package com.amc.service.alarm.impl;
+
+import java.util.List;
+
+import com.amc.common.Search;
+import com.amc.service.alarm.AlarmDAO;
+import com.amc.service.domain.Alarm;
+
+public class AlarmDAOImpl implements AlarmDAO {
+
+	@Override
+	public void addCancelAlarm(Alarm cancelAlarm) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addOpenAlarm(Alarm openAlarm) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Alarm> getCancelAlarmList(Search search) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Alarm> getOpenAlarmList(Search search) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteCancelAlarm(int cancelAlarmNo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteOpenAlarm(int openAlarmNo) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
